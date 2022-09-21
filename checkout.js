de4229cb286baf57894a95ad8ba62af4084ed29c
eyupGuc <meydanguresi@gmail.com>
2022-09-21 20:58:34 +0300

    capturing

--- checkout.js
+++ checkout.js
@@ -13,3 +13,26 @@ window.addEventListener("load", () => {
   localStorage.setItem("shippingPrice", shippingPrice);
   localStorage.setItem("shippingFreePrice", shippingFreePrice);
 });
+//Capturing vs. bubling
+
+const productsDiv = document.querySelector(".products");
+
+productsDiv.addEventListener("click", (event) => {
+  if (event.target.className == "fa-solid fa-minus") {
+    // console.log("minus btn is clicked!");
+    if (event.target.parentElement.querySelector(".quantity").innerText > 1) {
+      event.target.parentElement.querySelector(".quantity").innerText--;
+    } else {
+      if (confirm("Product will be remowed?")) {
+        event.target.parentElement.parentElement.parentElement.remove();
+      }
+    }
+  } else if (event.target.classList.contains("fa-plus")) {
+    event.target.previousElementSibling.innerText++;
+    // console.log("plus btn is clicked");
+  } else if (event.target.className == "remove-product") {
+    // console.log("remove btn is clicked");
+  } else {
+    // console.log("other element is clicked");
+  }
+});
